b73364979a17660ba8e4b933672c8928cb945248
Nikolaos Nikolaou <Nikolaounick@hotmail.com>
2018-02-02 17:12:42 +0200

    Update main.py

--- main.py
+++ main.py
@@ -14,27 +14,29 @@ def main():
     print(in_path)
     print(os.path.basename(os.path.normpath(in_path)))
 
+    # Open csv and reformat dots and commas
     # df = pd.DataFrame.from_csv(in_path, sep=';')
     df = pd.read_csv(in_path, delimiter=";", header=0)
     df[['Ποσό']] = [x.replace('.', '') for x in df['Ποσό']]
     df[['Ποσό']] = [x.replace(',', '.') for x in df['Ποσό']]
+    
+    # Convert to floats with prefix and datetime
     df[['Ποσό']] = df[['Ποσό']].astype(float)
+    df['Ποσό'] = df['Ποσό'].where(df['Πρόσημο ποσού'] == 'Π', other=-df['Ποσό'])
     df['Ημ/νία'] = pd.to_datetime(df['Ημ/νία'], format='%d/%m/%Y')
     # df = df.sort_values('Α/Α', ascending=False)
-    df = df.sort_values('Ημ/νία')
+    
+    # Check data types
     print(df.dtypes)
-
-    df['Ποσό'] = df['Ποσό'].where(df['Πρόσημο ποσού'] == 'Π', other=-df['Ποσό'])
+    
+    # Sort and create cumulative sum column
+    df = df.sort_values('Ημ/νία')
     df['cum_sum'] = df.Ποσό.cumsum()
     print(df)
 
     # df.plot(x='Ημ/νία', y='cum_sum', style='o')
-
     plt.plot(df['Ημ/νία'], df['cum_sum'])
     plt.show()
-
-
-
 
 
 if __name__ == "__main__":
