8ec529d60b9f71880269d7e773fbf5c34afbb57b
Nikolaos Nikolaou <Nikolaounick@hotmail.com>
2018-02-11 22:04:23 +0200

    Update main.py

--- main.py
+++ main.py
@@ -5,10 +5,10 @@ import os
 import pandas as pd
 import matplotlib.pyplot as plt
 import csv
+from sklearn import linear_model
 
 
 def main():
-
     tkinter.Tk().withdraw()  # Close the root window
     in_path = askopenfilename()
     print(in_path)
@@ -19,24 +19,41 @@ def main():
     df = pd.read_csv(in_path, delimiter=";", header=0)
     df[['Ποσό']] = [x.replace('.', '') for x in df['Ποσό']]
     df[['Ποσό']] = [x.replace(',', '.') for x in df['Ποσό']]
-    
+
     # Convert to floats with prefix and datetime
     df[['Ποσό']] = df[['Ποσό']].astype(float)
     df['Ποσό'] = df['Ποσό'].where(df['Πρόσημο ποσού'] == 'Π', other=-df['Ποσό'])
     df['Ημ/νία'] = pd.to_datetime(df['Ημ/νία'], format='%d/%m/%Y')
-    # df = df.sort_values('Α/Α', ascending=False)
-    
+
     # Check data types
     print(df.dtypes)
-    
+
     # Sort and create cumulative sum column
     df = df.sort_values('Ημ/νία')
     df['cum_sum'] = df.Ποσό.cumsum()
     print(df)
 
-    # df.plot(x='Ημ/νία', y='cum_sum', style='o')
+    df.plot(x='Ημ/νία', y='cum_sum', style='o')
     plt.plot(df['Ημ/νία'], df['cum_sum'])
     plt.show()
+
+    # define cumsum column as target var in a new df
+#     target = pd.DataFrame(data=df['cum_sum'], columns=["cum_sum"])
+#     dep = df[['Α/Α', 'Ποσό']].copy()
+#     print("target:")
+#     print(target)
+#     print(dep)
+#     xi = dep
+#     y = target["cum_sum"]
+
+#     lm = linear_model.LinearRegression()
+#     model = lm.fit(xi, y)
+
+    # The lm.fit() function fits a linear model.
+    # We  want to use the model to make predictions(that’s what we’re here for !), so we’ll use lm.predict():
+
+    # predictions = lm.predict(xi)
+    # print(predictions[0:5])
 
 
 if __name__ == "__main__":
